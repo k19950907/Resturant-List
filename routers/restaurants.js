84c151105656d5cb862cf084860a664e0d0fe962
k19950907 <k19950907@gmail.com>
2025-01-06 01:13:39 +0800

    bulid error messege handler

--- routers/restaurants.js
+++ routers/restaurants.js
@@ -10,103 +10,219 @@ const methodOverride = require('method-override')
 router.use(methodOverride('_method'))
 
 //list all restaurants 
+
 router.get("/", (req, res) => {
-  return Restaurant.findAll({
+  try {
+
+    return Restaurant.findAll({
     raw: true
+
   })
     .then((restaurants) => {
+
       res.render("index", { 
         restaurants, 
         successMessage: req.flash("success"), 
         failMessage: req.flash("fail") })
+
     })
-    .catch((err) => res.status(422).json(err))
+
+    .catch((error) => {
+
+      console.error(error)
+      req.flash("fail", "Loading page failed")
+      return res.redirect("back")
+
+    })
+
+  } catch(error) {
+
+    console.error(error)
+    req.flash("fail", "Loading page failed")
+    return res.redirect("back")
+
+  }
 })
 
 //page for creating restaurant
+
 router.get("/new", (req, res) => {
-  res.render("new", {})
+  try {
+
+    res.render("new", { failMessage: req.flash("fail") })
+
+  } catch(error) {
+
+    console.error(error)
+    req.flash("fail", "Loading page failed")
+    return res.redirect("back")
+
+  }
 })
 
 //creating restaurant
+
 router.post("/", (req, res) => {
-  return Restaurant.create(req.body)
-    .then(() => {
-      req.flash("success", "Creating new restaurant succeed")
-      res.redirect("/")
-    })
-    .catch((err) => {
-      req.flash("fail", "Creating new restaurant failed")
-      res.status(422).json(err)
-      res.redirect("/")
-    })
+  try {
+
+    return Restaurant.create(req.body)
+      .then(() => {
+
+        req.flash("success", "Creating new restaurant succeed")
+        res.redirect("/")
+      })
+      .catch((error) => {
+
+        console.error(error)
+        req.flash("fail", "Creating new restaurant failed")
+        return res.redirect("back")
+
+      })
+
+  } catch (error) {
+
+    console.log(error)
+    req.flash("fail", "Creating new restaurant failed")
+    return res.redirect("back")
+
+  }
 })
 
 //page for updating a restaurant
+
 router.get("/:id/edit", (req, res) => {
-  const id = req.params.id
-  return Restaurant.findByPk(id, {
-    raw: true
-  })
-    .then((restaurant) => res.render("update", { restaurant }))
-    .catch((err) => res.status(422).json(err))
+  try {
+
+    const id = req.params.id
+    return Restaurant.findByPk(id, {
+      raw: true
+    })
+
+      .then((restaurant) => {
+
+        res.render("update", { 
+          
+          restaurant,
+          failMessage: req.flash("fail")
+
+         })
+      })
+
+      .catch((error) => {
+
+        console.error(error)
+        req.flash("fail", "Updating restaurant failed")
+        return res.redirect("back")
+
+      })
+
+  } catch (error) {
+
+    console.log(error)
+    req.flash("fail", "Updating restaurant failed")
+    return res.redirect("back")
+
+  }
+  
 })
 
 //update restaurant
 router.put("/:id", (req, res) => {
-  const id = req.params.id
-  console.log(id)
-  const body = req.body
-  console.log(body)
-  return Restaurant.update({
-    name: body.name,
-    name_en: body.name_en,
-    category: body.category,
-    image: body.image,
-    location: body.location,
-    phone: body.phone,
-    google_map: body.google_map,
-    rating: body.rating,
-    description: body.description
-  }, { where: { id } })
-    .then(() => {
-      req.flash("success", "Updating restaurant succeed")
-      res.redirect(`/restaurants/${id}`)
-    })
-    .catch((err) => {
-      req.flash("fail", "Updating restaurant failed")
-      res.status(422).json(err)
-      res.redirect(`/restaurants/${id}`)
-    })
+  try {
+
+    const id = req.params.id
+    const body = req.body
+    return Restaurant.update({
+      name: body.name,
+      name_en: body.name_en,
+      category: body.category,
+      image: body.image,
+      location: body.location,
+      phone: body.phone,
+      google_map: body.google_map,
+      rating: body.rating,
+      description: body.description
+    }, { where: { id } })
+      .then(() => {
+        req.flash("success", "Updating restaurant succeed")
+        res.redirect(`/restaurants/${id}`)
+      })
+      .catch((error) => {
+        console.error(error)
+        req.flash("fail", "Updating restaurant failed")
+        return res.redirect("back")
+      })
+
+  } catch (error) {
+
+    console.log(error)
+    req.flash("fail", "Updating restaurant failed")
+    return res.redirect("back")
+
+  }
+  
 })
 
 //delete restaurant
+
 router.delete("/:id", (req, res) => {
-  const id = req.params.id
-  return Restaurant.destroy({ where: { id } })
-    .then(() => {
-      req.flash("success", "Deleting restaurant succeed")
-      res.redirect("/")
-    })
-    .catch((err) => {
-      req.flash("fail", "Deleting restaurant failed")
-      res.status(422).json(err)
-      res.redirect("/")
-    })
+  try {
+
+    const id = req.params.id
+    return Restaurant.destroy({ where: { id } })
+
+      .then(() => {
+        req.flash("success", "Deleting restaurant succeed")
+        res.redirect("/")
+
+      })
+
+      .catch((error) => {
+
+        console.error(error)
+        req.flash("fail", "Deleting restaurant failed")
+        return res.redirect("back")
+
+      })
+  } catch (error) {
+
+    console.log(error)
+    req.flash("fail", "Deleting restaurant failed")
+    return res.redirect("back")
+  } 
 })
 
 //show details
 router.get("/:id", (req, res) => {
-  const id = req.params.id
-  return Restaurant.findByPk(id, {
-    raw: true
-  })
-    .then((restaurant) => res.render("show", { 
-      restaurant, 
-      successMessage: req.flash("success"),
-      failMessage: req.flash("fail")
-    }))
-    .catch((err) => res.status(422).json(err))
+  try {
+
+    const id = req.params.id
+    return Restaurant.findByPk(id, {
+      raw: true
+
+    })
+
+      .then((restaurant) => res.render("show", {
+
+        restaurant,
+        successMessage: req.flash("success"),
+
+      }))
+      .catch((error) => {
+
+        console.error(error)
+        req.flash("fail", "Loading page failed")
+        return res.redirect("back")
+
+      })
+
+  } catch (error) {
+
+    console.error(error)
+    req.flash("fail", "Loading page failed")
+    return res.redirect("back")
+    
+  }
 })
 
 module.exports = router
